acd80c75619339b3150c72316565c9e63af770a1
James Davenport <jradavenport@gmail.com>
2021-12-01 14:28:54 -0800

    script reduce working at basic level

--- kosmos/wrappers.py
+++ kosmos/wrappers.py
@@ -7,20 +7,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.table import Table
 from astropy import units as u
+import datetime
 
-
-# __all__ = ['autoreduce', 'CoAddFinal', 'ReduceCoAdd', 'ReduceTwo']
+__all__ = ['script_reduce']
 
 def script_reduce(script,
                   trim=True,
                   apwidth=10, skysep=5, skywidth=5,
                   trace_nbins=15, trace_guess=None, trace_window=None,
                   stdtrace=False,
-                  Xfile='apoextinct.dat',
+                  obs_file='apoextinct.dat',
                   linelist='apohenear.dat',
                   waveapprox=False,
                   Saxis=0, Waxis=1,
-                  display=False, debug=False, silencewarnings=False):
+                  display=True, display_all=False,
+                  write_reduced=True,
+                  debug=False, silencewarnings=False):
     """
     Wrapper function that carries out all aspects of simple spectral
     reduction, according to a script. Here the script file is analogous to
@@ -56,15 +58,75 @@ def script_reduce(script,
         file6.fits, std:spec50cal/bd284211.dat
         file7.fits, object
 
+    Parameters
+    ----------
+    script : str
+        The .csv file to process (required)
+    trim : bool, optional (default is True)
+    apwidth : int, optional (default is 10)
+        The width along the spatial axis on either side of the trace to
+        extract. (passed to `apextract`)
+    skysep : int, optional (default is 5)
+        The separation in pixels from the aperture to the sky window.
+        (passed to `apextract`)
+    skywidth : int, optional (default is 5)
+        The width in pixels of the sky windows on either side of the
+        aperture. (passed to `apextract`)
+    trace_nbins : int, optional (default is 15)
+        number of bins in wavelength direction to chop image into.
+        (passed to `trace` as `nbins`)
+    trace_guess : int or None, optional (default is None)
+        A guess at where the desired trace is in the spatial direction. If set,
+        overrides the normal max peak finder. Good for tracing a fainter source if
+        multiple traces are present. (passed to `trace` as `guess`)
+    trace_window : int or None, optional (default is None)
+        If set, only fit the trace within a given region around the guess position.
+        Useful for tracing faint sources if multiple traces are present, but
+        potentially bad if the trace is substantially bent or warped.
+        (passed to `trace` as `window`)
+    stdtrace : bool, optional (default is False)
+        if True, use first object or standard to establish the trace, and use
+        for extracting every spectrum thereafter.
+    obs_file : str, optional (default is 'apoextinct.dat')
+        Observatory-specific airmass extinction file
+         passed to `obs_extinction`
+    linelist : str, optional (default is 'apohenear.dat')
+        Passed to `loadlinelist` to load arclines, which are used
+        by `identify_nearest`.
+    waveapprox : bool, optional (default is False)
+        if set, use the approximate wavelength from the header
+        'DISPDW' and 'DISPWC' keywords. Usually not great. 
+        (probably needs to be genearlized better)
+    Saxis : int, optional (default is 0)
+        Set which axis is the spatial dimension. For DIS, Saxis=0
+        (corresponds to NAXIS2 in header). For KOSMOS, Saxis=1.
+    Waxis : int, optional (default is 1)
+        Set which axis is the wavelength dimension. For DIS, Waxis=1
+        (corresponds to NAXIS1 in the header). For KOSMOS, Waxis=0.
+        NOTE: if Saxis is changed, Waxis will be updated, and visa versa.
+    display : bool, optional (default is True)
+        if set, plot reduced object and standard spectra to the screen
+    display_all : bool, optional (default is False)
+        if set, passes `display=True` to all other functions
+    write_reduced : bool, optional (default is True)
+        if set, write reduced object and standard spectra to a
+        .fits file, and make a log file for this run.
+    silencewarnings : bool, optional (default is False)
+        aggresively silence warnings that get spit out, mainly
+        from WCS. (caution: you might miss an important failure)
+
     Returns
     -------
     Output files are created for combined calibration frames
     (e.g. bias.fits, flat.fits), and fully reduced versions of objects
 
-    Add logs/intermediate files?
+    Improvements
+    ------------
+    - add other wavelenth solution methods (e.g. DTW based on Kosmos templates)
+    - Output reduced spectra using different file types? (text file?)
+    - enable jpeg save w/o display (faster/automatic)
 
     """
-
 
     if silencewarnings is True:
         # i hate doing this, but wcs/fits gives SO many warnings for our images...
@@ -81,7 +143,7 @@ def script_reduce(script,
     bias = None
     flat = None
     ilum = None
-    fallbacktrace=None
+    fallbacktrace = None
 
     # old DIS default was Saxis=0, Waxis=1, shape = (1028,2048)
     # KOSMOS is swapped, shape = (4096, 2148)
@@ -96,8 +158,28 @@ def script_reduce(script,
         arclines = kosmos.loadlinelist(linelist)
 
     # airmass observatory extinction file to use
-    if Xfile is not None:
-        Xfile = kosmos.obs_extinction(Xfile)
+    if obs_file is not None:
+        obs_file = kosmos.obs_extinction(obs_file)
+
+    # write the log file to save input settings
+    if write_reduced is True:
+        lout = open(script+'.log', 'w')
+        lout.write('# pykosmos/scriptreduce log for: ' + script +'\n')
+        now = datetime.datetime.now()
+        lout.write('DATE-REDUCED = ' + str(now) + '\n')
+        # save state of parameters
+        lout.write('trim = ' + str(trim) + '\n')
+        lout.write('trace_nbins = ' + str(trace_nbins) + '\n')
+        lout.write('trace_guess = ' + str(trace_guess) + '\n')
+        lout.write('trace_window = ' + str(trace_window) + '\n')
+        lout.write('stdtrace = ' + str(trace_window) + '\n')
+        lout.write('obs_file = ' + obs_file + '\n')
+        lout.write('linelist = ' + linelist + '\n')
+        lout.write('waveapprox = ' + str(waveapprox) + '\n')
+        lout.write('Saxis = ' + str(Saxis) + '\n')
+        lout.write('Waxis = ' + str(Waxis) + '\n')
+        lout.close() # close log file
+        # can move CLOSE to the end of script, in case there's things to add later
 
     j = 0
     while j < len(tbl):
@@ -157,7 +239,7 @@ def script_reduce(script,
                     print('using previous std trace')
                 trace = fallbacktrace
             else:
-                trace = kosmos.trace(img, display=display, nbins=trace_nbins,
+                trace = kosmos.trace(img, display=display_all, nbins=trace_nbins,
                                      guess=trace_guess, window=trace_window,
                                      Waxis=Waxis, Saxis=Saxis)
 
@@ -183,10 +265,10 @@ def script_reduce(script,
                 sci_fit = kosmos.fit_wavelength(spectrum, sci_xpts, sci_wpts, mode='interp', deg=3)
 
             # apply airmass calibration
-            if Xfile is not None:
+            if obs_file is not None:
                 ZD = img.header['ZD'] / 180.0 * np.pi
                 sci_airmass = 1.0 / np.cos(ZD)  # approximate Zenith Distance -> Airmass conversion
-                sci_fit = kosmos.airmass_cor(sci_fit, sci_airmass, Xfile)
+                sci_fit = kosmos.airmass_cor(sci_fit, sci_airmass, obs_file)
 
             if tbl['type'].data[j][0:3] == 'std':
                 # import standard star
@@ -199,22 +281,31 @@ def script_reduce(script,
                 standardstar = kosmos.onedstd(tbl['type'].data[j][4:])
                 # generate new sensitivity function
                 sensfunc = kosmos.standard_sensfunc(sci_fit, standardstar,
-                                                    mode='linear', display=display)
+                                                    mode='linear', display=display_all)
 
-            if tbl['type'].data[j] == 'object':
-                if debug:
-                    print(tbl['file'].data[j], ' object')
+            # if tbl['type'].data[j] == 'object':
+            #     if debug:
+            #         print(tbl['file'].data[j], ' object')
 
-                # flux calibration, apply sensfunc
-                final_spectrum = kosmos.apply_sensfunc(sci_fit, sensfunc)
+            # flux calibration, apply sensfunc (to either an Obj or Std)
+            final_spectrum = kosmos.apply_sensfunc(sci_fit, sensfunc)
 
-                plt.figure(figsize=(9, 4))
-                plt.plot(final_spectrum.wavelength, final_spectrum.flux, c='k', label='New reduction')
+            if display is True:
+                plt.figure()
+                plt.plot(final_spectrum.wavelength, final_spectrum.flux)
                 plt.xlabel('Wavelength [' + str(final_spectrum.wavelength.unit) + ']')
                 plt.ylabel('Flux [' + str(final_spectrum.flux.unit) + ']')
 
-                # write log file
-                # save 1D spectrum as some kind of ascii table
+                # save a jpeg version... b/c its nice for quick reference!
+                if write_reduced:
+                    plt.savefig(tbl['file'].data[j].split('.fits')[0]+'_reduced.jpeg',
+                                dpi=150, bbox_inches='tight', pad_inches=0.25)
+                plt.show()
+
+            # save 1D spectrum as some kind of ascii table
+            if write_reduced:
+                red_file = tbl['file'].data[j].split('.fits')[0]+'_reduced.fits'
+                final_spectrum.write(red_file, format='tabular-fits')
 
         j += 1
 
