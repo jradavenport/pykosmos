08fdb364945022bac3b57a26f8cfb3617488cc20
James Davenport <jradavenport@gmail.com>
2024-07-15 17:19:25 -0700

    move trace and extract to specreduce methods

--- pykosmos/apextract.py
+++ pykosmos/apextract.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-from scipy.interpolate import UnivariateSpline
 from specutils import Spectrum1D
-from astropy import units as u
+from astropy.nddata import CCDData
 from astropy.nddata import StdDevUncertainty
+from specreduce.tracing import FitTrace, ArrayTrace
+from astropy.modeling import models
+from specreduce.background import Background as Bkgd
+from specreduce.extract import BoxcarExtract as BExtract
+from warnings import warn
 
 __all__ = ['trace', 'BoxcarExtract']
 
@@ -36,7 +39,8 @@ def _gaus(x, a, b, x0, sigma):
 
 
 def trace(img, nbins=20, guess=None, window=None,
-          Saxis=0, Waxis=1, display=False, ax=None):
+          Saxis=0, Waxis=1, display=False, ax=None, 
+          trace_model=models.Spline1D(), return_trace=False):
     """
     Trace the spectrum aperture in an image
 
@@ -75,29 +79,28 @@ def trace(img, nbins=20, guess=None, window=None,
     ax : matplotlib axes or subplot object, optional
         axes or subplot to be plotted onto. If not specified one will be 
         created. (Default is None)
+    trace_model : astropy.modeling model, optional
+        passed to FitTrace (Default=models.Spline1D())
+    return_trace : bool, optional 
+        If True, return the FitTrace object. If False, return the np.ndarray,
+        as was done in previous versions. (Default is False)
 
     Returns
     -------
-    my : array
+    my : array or trace
         The spatial (Y) positions of the trace, interpolated over the
         entire wavelength (X) axis
 
     """
 
-    # Improvements Needed
-    # -------------------
-    # 1) switch to astropy models for Gaussian (?)
-    # 2) return info about trace width (?)
-    # 3) add re-fit trace functionality (or break off into another method)
-    # 4) add other interpolation modes besides spline, maybe via
-    #     specutils.manipulation methods?
-
-    # define the wavelength & spatial axis, if we want to enable swapping programatically later
-    # defined to agree with e.g.: img.shape => (1024, 2048) = (spatial, wavelength)
+    warn("PyKOSMOS trace is now a wrapper for functions within specreduce.tracing, and is kept for backwards compatibility. More features are available from specreduce.tracing.", 
+         DeprecationWarning, stacklevel=2)
 
     # Require at least 4 big bins along the trace to define shape. Sometimes can get away with very few
     if nbins < 4:
         raise ValueError('nbins must be >= 4')
+
+    img_to_trace = img
 
     # old DIS default was Saxis=0, Waxis=1, shape = (1028,2048)
     # KOSMOS is swapped, shape = (4096, 2148)
@@ -105,88 +108,20 @@ def trace(img, nbins=20, guess=None, window=None,
         # if either axis is swapped, swap them both to be sure!
         Saxis = 1
         Waxis = 0
+    
+        # if swapped (Saxis=1,Waxis=0), need to transpose before pass to specreduce trace,
+        # which doesn't support swapped axis
+        if isinstance(img, (CCDData)):
+            img_to_trace = img.data.T
+        else:
+            img_to_trace = img.T
 
-    # Pick the highest peak, bad if mult. obj. on slit...
-    ztot = np.nansum(img.data, axis=Waxis) / img.shape[Waxis]  # average image data across all wavelengths
-    yy = np.arange(len(ztot))
-    peak_y = np.nanargmax(ztot)
-
-    # if interact:
-    #     guess = ap_interac(yy, ztot)
-
-    # if the user set a guess for where the peak was, adopt that
-    if guess is not None:
-        peak_y = guess
-
-    # guess the peak width as the FWHM, roughly converted to gaussian sigma
-    width_guess = np.size(yy[ztot > (np.nanmax(ztot)/2.)]) / 2.355
-    # enforce some (maybe sensible?) rules about trace peak width
-    if width_guess < 2.:
-        width_guess = 2.
-    if width_guess > 25:
-        width_guess = 25
-
-    # [avg peak height, baseline, Y location of peak, width guess]
-    peak_guess = [np.nanmax(ztot), np.nanmedian(ztot), peak_y, width_guess]
-
-    # fit a Gaussian to peak for fall-back answer, but don't use yet
-    popt_tot, pcov = curve_fit(_gaus, yy, ztot, p0=peak_guess)
-
-    if window is not None:
-        ilum2 = yy[np.arange(peak_y-window, peak_y+window, dtype=int)]
-    else:
-        ilum2 = yy
-
-    xbins = np.linspace(0, img.shape[Waxis], nbins+1, dtype='int')
-    ybins = np.zeros(len(xbins)-1, dtype='float') * np.NaN
-
-    for i in range(0, len(xbins)-1):
-        # fit gaussian within each window
-        if Waxis == 1:
-            zi = np.nansum(img.data[ilum2, xbins[i]:xbins[i+1]], axis=Waxis)
-        if Waxis == 0:
-            zi = np.nansum(img.data[xbins[i]:xbins[i+1], ilum2], axis=Waxis)
-
-        peak_y = ilum2[np.nanargmax(zi)]
-        width_guess = np.size(ilum2[zi > (np.nanmax(zi) / 2.)]) / 2.355
-
-        if width_guess < 2.:
-            width_guess = 2.
-        if width_guess > 25:
-            width_guess = 25
-        pguess = [np.nanmax(zi), np.nanmedian(zi), peak_y, width_guess]
-        try:
-            popt, _ = curve_fit(_gaus, ilum2, zi, p0=pguess)
-
-            # if gaussian fits off chip, then fall back to previous answer
-            if (popt[2] <= min(ilum2)) or (popt[2] >= max(ilum2)):
-                ybins[i] = popt_tot[2]
-            else:
-                ybins[i] = popt[2]
-                popt_tot = popt  # if a good Gaussian fit, switch to these parameters for next fall-back
-
-        except RuntimeError:
-            popt = pguess
-
-    # recenter the bin positions
-    xbins = (xbins[:-1] + xbins[1:]) / 2.
-
-    yok = np.where(np.isfinite(ybins))[0]
-    if len(yok) > 0:
-        xbins = xbins[yok]
-        ybins = ybins[yok]
-
-        # run a cubic spline thru the bins
-        ap_spl = UnivariateSpline(xbins, ybins, k=3, s=0)
-
-        # interpolate the spline to 1 position per column
-        mx = np.arange(0, img.shape[Waxis])
-        my = ap_spl(mx)
-    else:
-        mx = np.arange(0, img.shape[Waxis])
-        my = np.zeros_like(mx) * np.NaN
-        import warnings
-        warnings.warn("TRACE ERROR: No Valid points found in trace")
+    # The Specreduce FitTrace is based on the original version of this function.
+    # Call this as a general purpose wrapper
+    tt = FitTrace(img_to_trace, bins=nbins, peak_method='gaussian', guess=guess, window=window, 
+                  trace_model=trace_model)
+    my = tt.trace.data
+    mx = np.arange(len(my))
 
     if display is True:
         if ax is None:
@@ -194,18 +129,24 @@ def trace(img, nbins=20, guess=None, window=None,
         im = ax.imshow(img, origin='lower', aspect='auto', cmap=plt.cm.Greys_r)
         im.set_clim(np.percentile(img, (5, 98)))
         if Waxis == 1:
-            ax.scatter(xbins, ybins, alpha=0.5)
+            # ax.scatter(xbins, ybins, alpha=0.5)
             ax.plot(mx, my)
         if Waxis == 0:
-            ax.scatter(ybins, xbins, alpha=0.5)
+            # ax.scatter(ybins, xbins, alpha=0.5)
             ax.plot(my, mx)
         plt.show()
 
-    return my
+    # if you want the output from FitTrace
+    if return_trace:
+        return tt
+    # otherwise just return the array, as the old behavior
+    else:
+        return my
 
 
 def BoxcarExtract(img, trace_line, apwidth=8, skysep=3, skywidth=7, skydeg=0,
-                  Saxis=0, Waxis=1, display=False, ax=None):
+                  Saxis=0, Waxis=1, display=False, ax=None,
+                  bkgd_sub=True, return_extract=False, return_sky=False):
     """
     This is nearly identical to specreduce.extract.BoxcarExtract,
     because that was based on the same PyDIS source code as this.
@@ -224,9 +165,10 @@ def BoxcarExtract(img, trace_line, apwidth=8, skysep=3, skywidth=7, skydeg=0,
     ----------
     img : CCDData object
         This is the image to run extract over
-    trace_line : 1-d array
+    trace_line : 1-d array or specreduce trace object
         The spatial positions (Y axis) corresponding to the center of the
-        trace for every wavelength (X axis), as returned from trace
+        trace for every wavelength (X axis), as returned from trace. Can be
+        either np.ndarray or a specreduce trace object.
     apwidth : int, optional
         The width along the Y axis on either side of the trace to extract.
         Note: a fixed width is used along the whole trace.
@@ -252,6 +194,24 @@ def BoxcarExtract(img, trace_line, apwidth=8, skysep=3, skywidth=7, skydeg=0,
     ax : matplotlib axes or subplot object, optional
         axes or subplot to be plotted onto. If not specified one will be 
         created. (Default is None)
+    bkgd_sub : bool, optional
+        If True, subtract the specreduce.background.Background from the 
+        image before running the extraction. This means the sky does not 
+        need to be subtracted later. (Default is True)
+    return_extract: bool, optional
+        If True, return the result of specreduce.extract.BoxcarExtract.
+        If False, return Spectrum1D objects for the extracted spectrum
+        and the skyspec estimated from Background. This is consistent
+        with previous function behavior. 
+        (Default is False)
+        NOTE: If `bkgd_sub=True` and  `return_extract=False` (default behavior) 
+        function will return skyspec, but user should probably NOT then 
+        subtract the sky from the spectrum again.
+    return_sky: bool, optional
+        If True, return sky (background) as well as spectrum
+        (Default is False)
+        NOTE: this default has changed, possibly breaking old code!
+        
 
     Returns
     -------
@@ -262,15 +222,9 @@ def BoxcarExtract(img, trace_line, apwidth=8, skysep=3, skywidth=7, skydeg=0,
 
     """
 
-    # Improvements Needed
-    # -------------------
-    # 1. take a wavelength solution for the trace, interpolate sky region
-    #     onto same wavelengths as the trace (BIG IMPROVEMENT)
-    #     Maybe use specutils.manipulation.FluxConservingResampler for the
-    #     interpolation over the sky regions?
-    # 2. optionally allow mode to be either simple aperture (current) or the
-    #     "optimal" (variance weighted) extraction algorithm
-
+    warn("PyKOSMOS BoxcarExtract is now a wrapper for functions within specreduce.extract.", 
+                  DeprecationWarning, stacklevel=2)
+    
     # old DIS default was Saxis=0, Waxis=1, shape = (1028,2048)
     # KOSMOS is swapped, shape = (4096, 2148)
     if (Saxis == 1) | (Waxis == 0):
@@ -285,90 +239,44 @@ def BoxcarExtract(img, trace_line, apwidth=8, skysep=3, skywidth=7, skydeg=0,
     if skywidth < 1:
         raise ValueError('skywidth must be >= 1')
 
-    onedspec = np.zeros_like(trace_line)
-    skysubflux = np.zeros_like(trace_line)
-    fluxerr = np.zeros_like(trace_line)
+    # need to convert trace_line into a new trace object
+    if isinstance(trace_line, (np.ndarray)):
+        trace_obj = ArrayTrace(img, trace_line)
+    else:
+        trace_obj = trace_line
 
-    for i in range(0, len(trace_line)):
-        # first do the aperture flux
-        # juuuust in case the trace gets too close to an edge
-        widthup = apwidth
-        widthdn = apwidth
-        if (trace_line[i]+widthup > img.shape[Saxis]):
-            widthup = img.shape[Saxis]-trace_line[i] - 1
-        if (trace_line[i]-widthdn < 0):
-            widthdn = trace_line[i] - 1
+    bg = Bkgd.two_sided(img, trace_obj, separation=skysep, width=skywidth, 
+                              disp_axis=Waxis, crossdisp_axis=Saxis)
 
-        # simply add up the total flux around the trace_line +/- width
-        if Saxis==0:
-            onedspec[i] = np.nansum(img.data[int(trace_line[i]-widthdn):int(trace_line[i]+widthup+1), i])
-        if Saxis==1:
-            onedspec[i] = np.nansum(img.data[i, int(trace_line[i] - widthdn):int(trace_line[i] + widthup + 1)])
+    skyspec = bg.bkg_spectrum()
 
-        # now do the sky fit
-        itrace_line = int(trace_line[i])
-        y = np.append(np.arange(itrace_line-apwidth-skysep-skywidth, itrace_line-apwidth-skysep),
-                      np.arange(itrace_line+apwidth+skysep+1, itrace_line+apwidth+skysep+skywidth+1))
+    img_to_ext = img
+    # should we subtract the background (sky) before extraction?
+    if bkgd_sub:
+        img_to_ext = img - bg
 
-        if Saxis == 0:
-            z = img.data[y, i]
-        if Saxis == 1:
-            z = img.data[i, y]
+    extr = BExtract(img_to_ext, trace_obj, width=apwidth, 
+                    disp_axis=Waxis, crossdisp_axis=Saxis)
+    # this is where we can add the Optimal Extraction option next
 
-        if skydeg>0:
-            # fit a polynomial to the sky in this column
-            pfit = np.polyfit(y, z, skydeg)
-            # define the aperture in this column
-            ap = np.arange(trace_line[i]-apwidth, trace_line[i]+apwidth+1)
-            # evaluate the polynomial across the aperture, and sum
-            skysubflux[i] = np.nansum(np.polyval(pfit, ap))
-        elif skydeg == 0:
-            skysubflux[i] = np.nanmean(z)*(apwidth*2.0 + 1)
+    # based on aperture phot err description by F. Masci, Caltech:
+    # http://wise2.ipac.caltech.edu/staff/fmasci/ApPhotUncert.pdf
+    sigB = 1 # stddev in the background data... not available yet from specreduce!
+    N_B = float(skywidth * 2)  # number of bkgd pixels
+    N_A = float(apwidth * 2) # number of aperture pixels
+    fluxerr = np.sqrt(extr.spectrum.flux.value + (N_A + N_A**2. / N_B) * (sigB**2.))
 
-        # finally, compute the error in this pixel
-        sigB = np.nanstd(z)  # stddev in the background data
-        N_B = float(len(y))  # number of bkgd pixels
-        N_A = apwidth * 2. + 1  # number of aperture pixels
-
-        # based on aperture phot err description by F. Masci, Caltech:
-        # http://wise2.ipac.caltech.edu/staff/fmasci/ApPhotUncert.pdf
-        fluxerr[i] = np.sqrt(np.nansum((onedspec[i]-skysubflux[i])) +
-                             (N_A + N_A**2. / N_B) * (sigB**2.))
-
-    if display:
-        if ax is None:
-            fig, ax = plt.subplots(1,1)
-        im = ax.imshow(img, origin='lower', aspect='auto', cmap=plt.cm.Greys_r)
-        im.set_clim(np.percentile(img, (5, 98)))
-
-        if Saxis == 0:
-            ax.plot(np.arange(len(trace_line)), trace_line, c='C0')
-            ax.fill_between(np.arange(len(trace_line)), trace_line + apwidth, trace_line-apwidth, color='C0', alpha=0.5)
-            ax.fill_between(np.arange(len(trace_line)), trace_line + apwidth + skysep, trace_line + apwidth + skysep + skywidth, color='C1', alpha=0.5)
-            ax.fill_between(np.arange(len(trace_line)), trace_line - apwidth - skysep, trace_line - apwidth - skysep - skywidth, color='C1', alpha=0.5)
-        if Saxis == 1:
-            ax.plot(trace_line, np.arange(len(trace_line)), c='C0')
-            ax.plot(trace_line + apwidth, np.arange(len(trace_line)), c='C0', alpha=0.5)
-            ax.plot(trace_line - apwidth, np.arange(len(trace_line)), c='C0', alpha=0.5)
-            ax.plot(trace_line - apwidth - skysep - skywidth, np.arange(len(trace_line)), c='C1', alpha=0.5)
-            ax.plot(trace_line - apwidth - skysep, np.arange(len(trace_line)), c='C1', alpha=0.5)
-            ax.plot(trace_line + apwidth + skysep + skywidth, np.arange(len(trace_line)), c='C1', alpha=0.5)
-            ax.plot(trace_line + apwidth + skysep, np.arange(len(trace_line)), c='C1', alpha=0.5)
-
-        plt.show()
-
-
-    spec = Spectrum1D(spectral_axis=np.arange(len(onedspec)) * u.pixel,
-                      flux=onedspec * img.unit,
-                      uncertainty=StdDevUncertainty(fluxerr)
+    spec = Spectrum1D(flux=extr.spectrum.flux, 
+                      uncertainty=StdDevUncertainty(fluxerr) 
                       )
-    skyspec = Spectrum1D(spectral_axis=np.arange(len(onedspec)) * u.pixel,
-                         flux=skysubflux * img.unit
-                         )
 
-    return spec, skyspec
-
-# work on a new extract that first de-warps the wavelength along the spatial axis...
-#    and maybe does something more PSF-like in the extraction
-
-# need to add "optimal extraction" method for weighting data vs sky
+    if return_extract:
+        if return_sky:
+            return extr, bg
+        else:
+            return extr
+    else:
+        if return_sky:
+            return spec, skyspec
+        else:
+            return spec
